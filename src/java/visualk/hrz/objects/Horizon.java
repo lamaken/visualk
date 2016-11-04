5a1e2fc463fab02ea978efa90fe82a5649790ef4
Alex Camps Mezquita <lamaken@gmail.com>
2016-11-04 19:24:13 +0100

    millores a saco passo nova versio

--- src/java/visualk/hrz/objects/Horizon.java
+++ src/java/visualk/hrz/objects/Horizon.java
@@ -22,7 +22,8 @@ import visualk.html5.UniqueName;
 
 public class Horizon implements Serializable {
 
-    private static final String URL_IMG = Main.HOST_NAME + Main.HOST_VISUALK + "/hrz/img/";
+    //private static final String URL_IMG = Main.HOST_NAME + Main.HOST_VISUALK + "/hrz/img/";
+    private static final String URL_IMG = "http://hrzmkr.com/img/";
 
     /**
      *
