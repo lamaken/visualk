f439223449620aaa2f8f7817511249c0ed41f031
Alex Camps Mezquita <lamaken@gmail.com>
2016-11-04 15:57:51 +0100

    refactor implements AuthAdapter

--- src/java/visualk/ss/db/Auth.java
+++ src/java/visualk/ss/db/Auth.java
@@ -1,81 +1,84 @@
 package visualk.ss.db;
 
 import java.sql.*;
+import visualk.db.AuthAdapter;
 
-public class Auth {
-    
-    private MysqlLayer mySQL=new MysqlLayer();
+public class Auth implements AuthAdapter {
+
+    private MysqlLayer mySQL;
     private ResultSet myResult;
-    
-  
-                     
-    public Auth(){
-    
+
+    public Auth(MysqlLayer mySQL, ResultSet myResult) {
+        this.mySQL = mySQL;
+        this.myResult = myResult;
     }
-  
-    public boolean canEnter(String alias, String clau){ 
-        this.myResult=mySQL.queryDB("select * from login where USER_NAME=\""+alias+"\" and USER_PASSW=\""+clau+"\"");
-        if(myResult!=null){
-            try{
-                if(this.myResult.first()){
-                	this.myResult.close();
-                	mySQL.dbConn.close();
-                	return true;
+
+    public boolean canEnter(String alias, String clau) {
+        this.myResult = mySQL.queryDB("select * from login where USER_NAME=\"" + alias + "\" and USER_PASSW=\"" + clau + "\"");
+        if (myResult != null) {
+            try {
+                if (this.myResult.first()) {
+                    this.myResult.close();
+                    mySQL.dbConn.close();
+                    return true;
+                } else {
+                    return false;
                 }
-                else return false;
-            }
-            catch (SQLException s)
-            {
-            	s.printStackTrace();
-            	System.out.println("Error en myResult");
-            }
-        }
-        return false;
-    }
-    public boolean isEmailActive(String email,String password){
-    	String sentence;
-    	sentence = "select * from usuaris where (passw is NULL or passw=\""+password+"\") and email=\""+email+"\"";
-    	System.out.print(sentence);
-    	this.myResult=mySQL.queryDB(sentence);
-        if(myResult!=null){
-            try{
-                if(this.myResult.first()){
-                	this.myResult.close();
-                	mySQL.dbConn.close();
-                	return true;
-                }
-                else return false;
-            }
-            catch (SQLException s)
-            {
-            	s.printStackTrace();
-            	System.out.println("Error en myResult");
+            } catch (SQLException s) {
+                s.printStackTrace();
+                System.out.println("Error en myResult");
             }
         }
         return false;
     }
 
-    public boolean isCodeActive(String code){
-    	String sentence;
-    	sentence = "select id_pub from publicacions where anonima=1 and codi_access=\""+code+"\" and dt_start<NOW() and NOW()<dt_end";
-    	
-    	this.myResult=mySQL.queryDB(sentence);
-        if(myResult!=null){
-            try{
-                if(this.myResult.first()){
-                	this.myResult.close();
-                	mySQL.dbConn.close();
-                	return true;
+    public boolean isEmailActive(String email, String password) {
+        String sentence;
+        sentence = "select * from usuaris where (passw is NULL or passw=\"" + password + "\") and email=\"" + email + "\"";
+        System.out.print(sentence);
+        this.myResult = mySQL.queryDB(sentence);
+        if (myResult != null) {
+            try {
+                if (this.myResult.first()) {
+                    this.myResult.close();
+                    mySQL.dbConn.close();
+                    return true;
+                } else {
+                    return false;
                 }
-                else return false;
-            }
-            catch (SQLException s)
-            {
-            	s.printStackTrace();
-            	System.out.println("Error en myResult");
+            } catch (SQLException s) {
+                s.printStackTrace();
+                System.out.println("Error en myResult");
             }
         }
         return false;
     }
-    
+
+    public boolean isCodeActive(String code) {
+        String sentence;
+        sentence = "select id_pub from publicacions where anonima=1 and codi_access=\"" + code + "\" and dt_start<NOW() and NOW()<dt_end";
+
+        this.myResult = mySQL.queryDB(sentence);
+        if (myResult != null) {
+            try {
+                if (this.myResult.first()) {
+                    this.myResult.close();
+                    mySQL.dbConn.close();
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (SQLException s) {
+                s.printStackTrace();
+                System.out.println("Error en myResult");
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void prepareDB(String dbServer, String dbUser, String dbPassword, String dbDataBase) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
