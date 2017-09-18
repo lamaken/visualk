fbae662f2121fc67d1d5d9e89360f344342ee9ad
Alex Camps Mezquita <lamaken@gmail.com>
2017-09-19 00:11:26 +0200

    svg a saco

--- web/art/cssAnime/js/script.js
+++ web/art/cssAnime/js/script.js
@@ -11,7 +11,7 @@ function init1(){
         var ball = document.createElement("img");
         ball.className="ball";
         var name = "rnd_"+999*Math.random();
-        ball.src="http://alkasoft.org/visualk/art/Arab?mx=10&my=10&cellw=5&rnd="+name;//Arab
+        ball.src="http://alkasoft.org/visualk/art/Arab?mx=10&my=10&cellw=3&rnd="+name;//Arab
         
         balls.appendChild(ball);
     }
@@ -28,7 +28,8 @@ function init(){
     
     for (var n=0;n<100;n++)
     {    
-     str += "<img class=ball src='http://alkasoft.org/visualk/art/Arab?mx=100&my=100&cellw=50&rnd="+n+"'/>";//Arab
+     var name = "rnd_"+999*Math.random();   
+     str += "<img class=ball src='http://alkasoft.org/visualk/art/graph/LiveMosaic?mx=100&my=100&cellw=35&rnd="+name+"'/>";//Arab
     }
     balls.innerHTML=str;
 }
